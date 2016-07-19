bba89cb7a1fe0469c310ab87204720f86d15538e
Mike Eltsufin <meltsufin@google.com>
2016-07-19 14:01:26 -0400

    fix deploy

--- test.sh
+++ test.sh
@@ -48,8 +48,12 @@ testDeploy ()
   local pom=${project}/pom.xml
   local url=http://${project}-dot-${gcpproject}.appspot.com/test
 
+  if [ $buildTool = "maven" ]; then
+    deploy="mvn -f ${project}/pom.xml clean appengine:delploy"
+  fi
+
   echo "START DEPLOY $project"
-  if mvn -f $pom clean appengine:deploy &> /dev/null \
+  if eval $deploy &> /dev/null \
     && curl --silent $url | grep -z $pattern &> /dev/null
   then
     echo $project is up
@@ -69,17 +73,17 @@ testDeploy ()
 testDevserver '1-standard' 'Hello.*FilePermission'
 testDevserver '4-java8-compat' 'Hello.*Flex'
 
-#testDeploy '1-standard' 'Hello.*FilePermission' \
-#& testDeploy '2-java7' 'Hello.*Flex' \
-#& testDeploy '3-java7-extended' 'Hello.*Flex' \
-#& testDeploy '4-java8-compat' 'Hello.*Flex' \
-#& testDeploy '5-java8-compat-extended' 'Hello.*Flex' \
-#& testDeploy '6-java8-jetty9' 'Hello.*Flex' \
-#& testDeploy '7-java8-jetty9-extended' 'Hello.*Flex' \
-#& testDeploy '8-java8' 'Hello.*Flex' \
-#& testDeploy '9-java8-compat-flex' 'Hello.*Flex' \
-#& testDeploy '10-java8-flex' 'Hello.*Flex' \
-#& wait && echo DONE SUCCESS || echo DONE FAILURE
+testDeploy '1-standard' 'Hello.*FilePermission' \
+& testDeploy '2-java7' 'Hello.*Flex' \
+& testDeploy '3-java7-extended' 'Hello.*Flex' \
+& testDeploy '4-java8-compat' 'Hello.*Flex' \
+& testDeploy '5-java8-compat-extended' 'Hello.*Flex' \
+& testDeploy '6-java8-jetty9' 'Hello.*Flex' \
+& testDeploy '7-java8-jetty9-extended' 'Hello.*Flex' \
+& testDeploy '8-java8' 'Hello.*Flex' \
+& testDeploy '9-java8-compat-flex' 'Hello.*Flex' \
+& testDeploy '10-java8-flex' 'Hello.*Flex' \
+& wait && echo DONE SUCCESS || echo DONE FAILURE
 
 kill 0
 
