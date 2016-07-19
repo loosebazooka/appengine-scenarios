e341734d1fd2e6544d66a60645eae8d916e5edbb
Appu Goundan <appu@google.com>
2016-07-19 13:45:48 -0400

    test script with artifact name fixes

--- test.sh
+++ test.sh
@@ -4,16 +4,29 @@
 #set -x
 
 JAVA_HOME=/usr/local/buildtools/java/jdk8
-gcpproject='eltsufin-sandbox'
+
+# get cloud project from inputs
+if [ "$#" -ne 1 ]; then
+  echo "usage $0 <cloud-project> <gradle/maven>"
+  exit 1
+fi 
+gcpproject=$1
+buildTool=$2
+
+if [ $buildTool -eq "maven" ]; then
+  devAppServerStart="mvn -f ${project}/pom.xml clean appengine:start"
+  devAppServerStop="mvn -f $pom appengine:stop"
+fi
 
 testDevserver ()
 {
   local project=$1
   local pattern=$2
-  local pom=${project}/pom.xml
 
-  mvn -f $pom clean appengine:run &> /dev/null &
-  sleep 4
+  #local pom=${project}/pom.xml
+  #mvn -f $pom clean appengine:start &> /dev/null
+  eval "${devAppServerStart} &> /dev/null"
+
   if curl --silent http://localhost:8080/test | grep -z $pattern &> /dev/null
   then
     echo "DEVSERVER $project is up" 
@@ -22,7 +35,8 @@ testDevserver ()
     return 1
   fi
 
-  mvn -f $pom appengine:stop &> /dev/null 
+  #mvn -f $pom appengine:stop &> /dev/null 
+  eval "${devAppServerStop}" &> /dev/null 
   echo "DEVSERVER PASS $project"
 
   return 0
@@ -37,9 +51,7 @@ testDeploy ()
 
   echo "START DEPLOY $project"
   if mvn -f $pom clean appengine:deploy &> /dev/null \
-    && sleep 10 \
-    && curl --silent $url \
-    | grep -z $pattern &> /dev/null
+    && curl --silent $url | grep -z $pattern &> /dev/null
   then
     echo $project is up
   else
@@ -58,17 +70,17 @@ testDeploy ()
 testDevserver '1-standard' 'Hello.*FilePermission'
 testDevserver '4-java8-compat' 'Hello.*Flex'
 
-testDeploy '1-standard' 'Hello.*FilePermission' \
-& testDeploy '2-java7' 'Hello.*Flex' \
-& testDeploy '3-java7-extended' 'Hello.*Flex' \
-& testDeploy '4-java8-compat' 'Hello.*Flex' \
-& testDeploy '5-java8-compat-extended' 'Hello.*Flex' \
-& testDeploy '6-java8-jetty9' 'Hello.*Flex' \
-& testDeploy '7-java8-jetty9-extended' 'Hello.*Flex' \
-& testDeploy '8-java8' 'Hello.*Flex' \
-& testDeploy '9-java8-compat-flex' 'Hello.*Flex' \
-& testDeploy '10-java8-flex' 'Hello.*Flex' \
-& wait && echo DONE SUCCESS || echo DONE FAILURE
+#testDeploy '1-standard' 'Hello.*FilePermission' \
+#& testDeploy '2-java7' 'Hello.*Flex' \
+#& testDeploy '3-java7-extended' 'Hello.*Flex' \
+#& testDeploy '4-java8-compat' 'Hello.*Flex' \
+#& testDeploy '5-java8-compat-extended' 'Hello.*Flex' \
+#& testDeploy '6-java8-jetty9' 'Hello.*Flex' \
+#& testDeploy '7-java8-jetty9-extended' 'Hello.*Flex' \
+#& testDeploy '8-java8' 'Hello.*Flex' \
+#& testDeploy '9-java8-compat-flex' 'Hello.*Flex' \
+#& testDeploy '10-java8-flex' 'Hello.*Flex' \
+#& wait && echo DONE SUCCESS || echo DONE FAILURE
 
 kill 0
 
