6708e782efd93d8c087cd7cbaf16e7d60cc5aa71
Appu Goundan <appu@google.com>
2016-07-19 15:45:32 -0400

    more test.sh changes

--- test.sh
+++ test.sh
@@ -4,6 +4,10 @@
 #set -x
 
 JAVA_HOME=/usr/local/buildtools/java/jdk8
+testOutput="test_output.txt"
+
+# clear testOutput file
+echo "" > $testOutput
 
 # get cloud project from inputs
 if [ "$#" -ne 2 ]; then
@@ -12,6 +16,11 @@ if [ "$#" -ne 2 ]; then
 fi 
 gcpproject=$1
 buildTool=$2
+
+if [ $buildTool != "gradle" ] && [ $buildTool != "maven" ]; then
+  echo "bad build tool: $buildTool, specify gradle or maven"
+  exit 1
+fi
 
 
 testDevserver ()
@@ -22,9 +31,12 @@ testDevserver ()
   if [ $buildTool = "maven" ]; then
     devAppServerStart="mvn -f ${project}/pom.xml clean appengine:start"
     devAppServerStop="mvn -f ${project}/pom.xml appengine:stop"
+  else # buildtool = "gradle"
+    devAppServerStart="( cd ${project} && ./gradlew clean appengineStart )"
+    devAppServerStop="( cd ${project} && ./gradlew appengineStop )"
   fi
 
-  eval ${devAppServerStart} &> /dev/null
+  eval "${devAppServerStart} >> ${testOutput} 2>&1"
 
   if curl --silent http://localhost:8080/test | grep -z $pattern &> /dev/null
   then
@@ -35,6 +47,8 @@ testDevserver ()
   fi
 
   eval $devAppServerStop &> /dev/null 
+  # need to sleep a little for server to shutdown
+  sleep 4
 
   echo "DEVSERVER PASS $project"
 
@@ -49,18 +63,26 @@ testDeploy ()
   local url=http://${project}-dot-${gcpproject}.appspot.com/test
 
   if [ $buildTool = "maven" ]; then
-    deploy="mvn -f ${project}/pom.xml clean appengine:delploy"
+    deploy="mvn -f ${project}/pom.xml clean appengine:deploy"
+  else # buildtool = "gradle"
+    deploy="( cd ${project} && ./gradlew clean appengineDeploy )"
   fi
 
   echo "START DEPLOY $project"
-  if eval $deploy &> /dev/null \
-    && curl --silent $url | grep -z $pattern &> /dev/null
+  if eval "$deploy >> ${testOutput} 2>&1" \
+    && curl --silent $url \
+    && curlOut=$(curl --silent $url) \
+    && echo $curlOut | grep -z $pattern &> /dev/null
   then
     echo $project is up
   else
-    echo "*** FAIL ***: $pattern not found in $project"
+    echo "** DEPLOY FAIL $project: $pattern not found in $project"
+    echo $curlOut
+    echo "goose"
+    curlOut2=$(curl --silent $url)
+    echo $curlOut2
     echo $url
-    curl --silent $url
+#    curl --silent $url
     return 1
   fi
 
@@ -70,20 +92,20 @@ testDeploy ()
   return 0
 }
 
-testDevserver '1-standard' 'Hello.*FilePermission'
+#testDevserver '1-standard' 'Hello.*FilePermission'
 testDevserver '4-java8-compat' 'Hello.*Flex'
 
-testDeploy '1-standard' 'Hello.*FilePermission' \
-& testDeploy '2-java7' 'Hello.*Flex' \
-& testDeploy '3-java7-extended' 'Hello.*Flex' \
-& testDeploy '4-java8-compat' 'Hello.*Flex' \
-& testDeploy '5-java8-compat-extended' 'Hello.*Flex' \
-& testDeploy '6-java8-jetty9' 'Hello.*Flex' \
-& testDeploy '7-java8-jetty9-extended' 'Hello.*Flex' \
-& testDeploy '8-java8' 'Hello.*Flex' \
-& testDeploy '9-java8-compat-flex' 'Hello.*Flex' \
-& testDeploy '10-java8-flex' 'Hello.*Flex' \
-& wait && echo DONE SUCCESS || echo DONE FAILURE
+#testDeploy '1-standard' 'Hello.*FilePermission' \
+#& testDeploy '2-java7' 'Hello.*Flex' \
+#& testDeploy '3-java7-extended' 'Hello.*Flex' \
+#& testDeploy '4-java8-compat' 'Hello.*Flex' \
+#& testDeploy '5-java8-compat-extended' 'Hello.*Flex' \
+#& testDeploy '6-java8-jetty9' 'Hello.*Flex' \
+#& testDeploy '7-java8-jetty9-extended' 'Hello.*Flex' \
+#& testDeploy '8-java8' 'Hello.*Flex' \
+#testDeploy '9-java8-compat-flex' 'Hello.*Flex' \
+#& testDeploy '10-java8-flex' 'Hello.*Flex' \
+#& wait && echo DONE SUCCESS || echo DONE FAILURE
 
 kill 0
 
