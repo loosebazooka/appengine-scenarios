e4a14bba3ca76601e8097fd4dcd58edd7a00b316
Mike Eltsufin <meltsufin@google.com>
2016-07-20 14:35:26 -0400

    test.sh updates

--- test.sh
+++ test.sh
@@ -4,10 +4,7 @@
 #set -x
 
 JAVA_HOME=/usr/local/buildtools/java/jdk8
-testOutput="test_output.txt"
 
-# clear testOutput file
-echo "" > $testOutput
 
 # get cloud project from inputs
 if [ "$#" -ne 2 ]; then
@@ -27,7 +24,8 @@ testDevserver ()
 {
   local project=$1
   local pattern=$2
-
+  local url=http://localhost:8080/test
+  
   if [ $buildTool = "maven" ]; then
     devAppServerStart="mvn -f ${project}/pom.xml clean appengine:start"
     devAppServerStop="mvn -f ${project}/pom.xml appengine:stop"
@@ -36,20 +34,19 @@ testDevserver ()
     devAppServerStop="( cd ${project} && ./gradlew appengineStop )"
   fi
 
-  eval "${devAppServerStart} >> ${testOutput} 2>&1"
-
-  if curl --silent http://localhost:8080/test | grep -z $pattern &> /dev/null
+  if eval ${devAppServerStart} &> /dev/null \
+    && curl --silent $url | grep -z "$pattern" &> /dev/null
   then
-    echo "DEVSERVER $project is up" 
+    echo "DEVSERVER $project is up"
   else
     echo "*** FAIL ***: DEVSERVER $project didn't come up"
+    curl $url
     return 1
   fi
 
   eval $devAppServerStop &> /dev/null 
   # need to sleep a little for server to shutdown
-  sleep 4
-
+  sleep 2
   echo "DEVSERVER PASS $project"
 
   return 0
@@ -69,20 +66,14 @@ testDeploy ()
   fi
 
   echo "START DEPLOY $project"
-  if eval "$deploy >> ${testOutput} 2>&1" \
-    && curl --silent $url \
-    && curlOut=$(curl --silent $url) \
-    && echo $curlOut | grep -z $pattern &> /dev/null
+  if eval $deploy &> /dev/null \
+    && curl --silent $url  && curl --silent $url | grep -z "$pattern" &> /dev/null
   then
     echo $project is up
   else
-    echo "** DEPLOY FAIL $project: $pattern not found in $project"
-    echo $curlOut
-    echo "goose"
-    curlOut2=$(curl --silent $url)
-    echo $curlOut2
+    echo "*** FAIL ***: DEPLOY FAILED $project: "$pattern" not found in $project"
     echo $url
-#    curl --silent $url
+    curl --silent $url
     return 1
   fi
 
@@ -93,19 +84,25 @@ testDeploy ()
 }
 
 #testDevserver '1-standard' 'Hello.*FilePermission'
-testDevserver '4-java8-compat' 'Hello.*Flex'
+#testDevserver '2-java7' 'Hello.*Flex'
+#testDevserver '3-java7-extended' 'Hello.*Flex'
+#testDevserver '4-java8-compat' 'Hello.*Flex'
+#testDevserver '5-java8-compat-extended' 'Hello.*Flex'
+#testDevserver '9-java8-compat-flex' 'Hello.*Flex'
 
 #testDeploy '1-standard' 'Hello.*FilePermission' \
 #& testDeploy '2-java7' 'Hello.*Flex' \
-#& testDeploy '3-java7-extended' 'Hello.*Flex' \
+#& testDeploy '3-java7-extended' 'Hello.*Custom Flex' \
 #& testDeploy '4-java8-compat' 'Hello.*Flex' \
-#& testDeploy '5-java8-compat-extended' 'Hello.*Flex' \
+#& testDeploy '5-java8-compat-extended' 'Hello.*Custom Flex' \
 #& testDeploy '6-java8-jetty9' 'Hello.*Flex' \
-#& testDeploy '7-java8-jetty9-extended' 'Hello.*Flex' \
+#& testDeploy '7-java8-jetty9-extended' 'Hello.*Custom Flex' \
 #& testDeploy '8-java8' 'Hello.*Flex' \
 #testDeploy '9-java8-compat-flex' 'Hello.*Flex' \
 #& testDeploy '10-java8-flex' 'Hello.*Flex' \
-#& wait && echo DONE SUCCESS || echo DONE FAILURE
+#& wait && echo DONE
+
+testDeploy '3-java7-extended' 'Hello.*1.7.*Custom Flex' 
 
 kill 0
 
